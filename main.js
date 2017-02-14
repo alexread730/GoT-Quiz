be3635146f734d5ad97e7900a9ab9c1fcf0ee01d
Alex Read <alexread730@gmail.com>
2017-02-13 19:25:32 -0600

    Fully functioning app

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	var i = -1;
 	var qNum = 0;
 	var score = 0;
+	var toggleElements = $(".radbut, br, #next, #R1, #R2, #R3, #R4");
 	$(".radbut").hide();
 	$("br").hide();
 
@@ -20,14 +21,17 @@ $(document).ready(function() {
 
 			var value = ($('input[name="Q"]:checked').prop('checked', false).val()); //checks which radio button is checked and sets it to value
 				//add +1 to score if choice matches answer
-				if (i >= 0) {
+				if (i >= 0 && i < 4) {
 					if (value == allQuestions[i].answer) {
 						score++;
 					};
 			};
 
+       		
 			i += 1;
 			qNum += 1;
+
+			toggleElements[allQuestions[i].choices ? 'show' : 'hide']();
 			
 			//display questions and variables
 			$("#questionLoc").text(allQuestions[i].question);
@@ -39,11 +43,18 @@ $(document).ready(function() {
 			$("#R4").text(allQuestions[i].choices[3]);
 		}
 
+		 /* Show these for values
 			$("#qNum").text(qNum);
 			$("#score").text(score);
 			$("#quesVal").text(allQuestions[i].answer);
+		*/
+		
+			if (i >= 3) {
+				$("h3").text("Winter has come! You have scored " +score+ "/3.");
+			}			
 
-			
+
+
 		});
 });
 
@@ -58,7 +69,7 @@ function question(question, choices, answer) {
 var question1 = new question("Who is Sansa\'s mother?", ["Cercei", "Marjorie", "Catelin", "Lysa"], 3);
 var question2 = new question("Where are the Starks from?", ["Winterfell", "King's Landing", "Valyria", "Dorne"], 1);
 var question3 = new question("Who is the biggest asshole in this group?", ["Eddard", "Khaleesi", "Bran", "Geoffrey"], 4);
-var endSent = new question("End");
+var endSent = new question(" ");
 
 allQuestions.push(question1, question2, question3, endSent);
 
